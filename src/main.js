0a2c3848d2c0d651407eacff00270c497b9ae58f
Roberto Kennedy <roberto.kpp@gmail.com>
2025-10-30 21:15:43 -0500

    toda configuracao finalizada

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// configuracao do dayjs
+import "./libs/dayjs"
+
+
+//css
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("HH:mm"))
